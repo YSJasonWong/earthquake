67d4601f03d9c55bc5a4d9014444f8c73f6ab5c5
Jason Wong <73357612+YSJasonWong@users.noreply.github.com>
2022-03-23 03:13:35 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ fetch('https://data.weather.gov.hk/weatherAPI/opendata/earthquake.php?dataType=q
   .then(k => data=k);
 
 function preload() {
-  bg = loadImage('world (1).svg');
+  bg = loadImage('world.svg');
 }
 
 function setup() {
@@ -20,16 +20,14 @@ function draw() {
   textSize(width*0.05);
   textAlign(CENTER, CENTER);
   text(`Last Earthquake Occured`, width/2, height*0.1);
-  let x = map(data.lon, -180, 180, 0, width);
-  let y = map(data.lat, -90, 90, height/2+width/ratio/2, height/2-width/ratio/2);
   fill(255, 0, 0);
   stroke(255, 0, 0);
-  ellipse(x, y, width*0.01);
+  ellipse(getX(data.lon), getY(data.lat), width*0.01);
   let maxRad = map(data.mag, 6, 9, width*0.1, width*0.3);
   rad = (rad+1)%maxRad;
   noFill();
   for (let i = 0; i<5; i++) {
-    ellipse(x, y, rad/i);
+    ellipse(getX(data.lon), getY(data.lat), rad/i);
   }
   fill(255);
   noStroke();
@@ -38,4 +36,12 @@ function draw() {
   text(`Last update: ${data.updateTime}`, 10, height-10);
   textAlign(RIGHT, BOTTOM);
   text(`Region: ${data.region}, Magitude: ${data.mag}, ${data.ptime}`, width-10, height-10);
+}
+
+function getX(input) {
+  return map(input, -180, 180, -width*0.05, width);
+}
+
+function getY(input) {
+  return map(input, -90, 90, height/2+width/ratio*0.35, height/2-width/ratio*0.35);
 }
